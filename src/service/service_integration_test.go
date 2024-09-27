9285030bd15a7c5040b6c1f37922ae47e91c517a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-09-27 01:28:29 -0300

    feat: create `HiringProcessCandidate` related integration tests

---
+++ src/service/service_integration_test.go
@@ -0,0 +1,102 @@
+//go:build integration
+// +build integration
+
+package service
+
+import (
+	"context"
+	"testing"
+
+	"api5back/ent/facthiringprocess"
+	"api5back/seeds"
+	"api5back/src/database"
+	"api5back/src/property"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDatabaseOperations(t *testing.T) {
+	ctx := context.Background()
+	var intEnv *database.IntegrationEnvironment = nil
+
+	if testResult := t.Run("Setup database connection", func(t *testing.T) {
+		intEnv = database.DefaultIntegrationEnvironment(ctx).
+			WithSeeds(seeds.DataWarehouse)
+
+		require.NotNil(t, intEnv)
+		require.NoError(t, intEnv.Error)
+		require.NotNil(t, intEnv.Client)
+	}); !testResult {
+		t.Fatalf("Setup test failed")
+	}
+
+	t.Run("Test hiring_process_candidate table operations", func(t *testing.T) {
+		var testFactHiringProcessId int
+
+		for _, TestCase := range []database.TestCase{
+			{
+				Name: "Insert a hiring_process_candidate into the table",
+				Run: func(t *testing.T) {
+					factHiringProcess, err := intEnv.
+						Client.
+						FactHiringProcess.
+						Query().
+						WithDimVacancy().
+						First(ctx)
+					require.NoError(t, err)
+
+					testFactHiringProcessId = factHiringProcess.ID
+
+					dimVacancy, err := factHiringProcess.
+						Edges.
+						DimVacancyOrErr()
+					require.NoError(t, err)
+
+					applyDatePgType := &pgtype.Date{}
+					if err := applyDatePgType.Scan(dimVacancy.OpeningDate); err != nil {
+						t.Fatalf("failed to parse applyDatePgType for candidate: %v", err)
+					}
+
+					_, err = intEnv.Client.HiringProcessCandidate.
+						Create().
+						SetFactHiringProcessID(testFactHiringProcessId).
+						SetName("John Doe").
+						SetEmail("John@Doe.com").
+						SetPhone("+1234567890").
+						SetApplyDate(applyDatePgType).
+						SetStatus(property.HiringProcessCandidateStatusInAnalysis).
+						SetScore(0).
+						Save(ctx)
+					if err != nil {
+						t.Fatalf("failed to insert the hiring_process_candidate: %v", err)
+					}
+				},
+			},
+			{
+				Name: "Select candidate list from the edges of a FactHiringProcess",
+				Run: func(t *testing.T) {
+					factHiringProcesses, err := intEnv.
+						Client.
+						FactHiringProcess.
+						Query().
+						WithDimVacancy().
+						Where(facthiringprocess.ID(testFactHiringProcessId)).
+						First(ctx)
+					require.NoError(t, err)
+
+					candidates, err := factHiringProcesses.
+						Edges.
+						HiringProcessCandidatesOrErr()
+					require.NoError(t, err)
+					require.NotNil(t, candidates)
+					require.NotEmpty(t, candidates)
+				},
+			},
+		} {
+			if testResult := t.Run(TestCase.Name, TestCase.Run); !testResult {
+				t.Fatalf("Test case failed")
+			}
+		}
+	})
+}
