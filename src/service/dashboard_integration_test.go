37a267e4762fed6502923eb157854b31f148be09
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-11-13 19:36:52 -0300

    test: reorganize `database` and `service` integration tests

--- src/service/dashboard_integration_test.go
+++ src/service/dashboard_integration_test.go
@@ -7,16 +7,14 @@ import (
 	"context"
 	"testing"
 
-	"api5back/ent/facthiringprocess"
 	"api5back/seeds"
 	"api5back/src/database"
 	"api5back/src/model"
-	"api5back/src/property"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestDatabaseOperations(t *testing.T) {
+func TestDashboard(t *testing.T) {
 	ctx := context.Background()
 	var intEnv *database.IntegrationEnvironment = nil
 
@@ -30,91 +28,6 @@ func TestDatabaseOperations(t *testing.T) {
 	}); !testResult {
 		t.Fatalf("Setup test failed")
 	}
-
-	t.Run("Test hiring_process_candidate table operations", func(t *testing.T) {
-		var testFactHiringProcessId int
-		var hiringProcessCandidateId int
-
-		for _, TestCase := range []database.TestCase{
-			{
-				Name: "Insert a hiring_process_candidate into the table",
-				Run: func(t *testing.T) {
-					factHiringProcess, err := intEnv.
-						Client.
-						FactHiringProcess.
-						Query().
-						WithDimVacancy().
-						First(ctx)
-					require.NoError(t, err)
-
-					testFactHiringProcessId = factHiringProcess.ID
-
-					dimVacancy, err := factHiringProcess.
-						Edges.
-						DimVacancyOrErr()
-					require.NoError(t, err)
-
-					hiringProcessCandidate, err := intEnv.Client.HiringProcessCandidate.
-						Create().
-						SetFactHiringProcessID(testFactHiringProcessId).
-						SetDbId(1).
-						SetName("John Doe").
-						SetEmail("John@Doe.com").
-						SetPhone("+1234567890").
-						SetApplyDate(dimVacancy.OpeningDate).
-						SetStatus(property.HiringProcessCandidateStatusInAnalysis).
-						SetScore(0).
-						Save(ctx)
-					if err != nil {
-						t.Fatalf("failed to insert the hiring_process_candidate: %v", err)
-					}
-
-					hiringProcessCandidateId = hiringProcessCandidate.ID
-				},
-			},
-			{
-				Name: "Select candidate list from the edges of a FactHiringProcess",
-				Run: func(t *testing.T) {
-					factHiringProcesses, err := intEnv.
-						Client.
-						FactHiringProcess.
-						Query().
-						WithDimVacancy().
-						WithHiringProcessCandidates().
-						Where(facthiringprocess.ID(testFactHiringProcessId)).
-						First(ctx)
-					require.NoError(t, err)
-
-					candidates, err := factHiringProcesses.
-						Edges.
-						HiringProcessCandidatesOrErr()
-					require.NoError(t, err)
-					require.NotNil(t, candidates)
-					require.NotEmpty(t, candidates)
-				},
-			},
-			{
-				Name: "Select a candidate by ID",
-				Run: func(t *testing.T) {
-					hiringProcessCandidate, err := intEnv.
-						Client.
-						HiringProcessCandidate.
-						Get(ctx, hiringProcessCandidateId)
-					require.NoError(t, err)
-					require.NotNil(t, hiringProcessCandidate)
-					require.Equal(
-						t,
-						property.HiringProcessCandidateStatusInAnalysis,
-						hiringProcessCandidate.Status,
-					)
-				},
-			},
-		} {
-			if testResult := t.Run(TestCase.Name, TestCase.Run); !testResult {
-				t.Fatalf("Test case failed")
-			}
-		}
-	})
 
 	if testResult := t.Run("GetMetrics returns correct metrics", func(t *testing.T) {
 		metricsData, err := GetMetrics(
