d6540d8811875a81bd0525c802c70af02269047a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-11-21 06:05:30 -0300

    test: create `ParseOffsetAndTotalPages` unit tests in `pagination` module

--- src/pagination/pagination_test.go
+++ src/pagination/pagination_test.go
@@ -95,6 +95,91 @@ func TestParsePageRequest(t *testing.T) {
 	} {
 		if testResult := t.Run(testCase.Name, testCase.Run); !testResult {
 			t.Fatalf("Test case failed")
+
+func TestParseOffsetAndTotalPages(t *testing.T) {
+	type TestCase struct {
+		Name               string
+		Page               int
+		PageSize           int
+		TotalItems         int
+		ExpectedOffset     int
+		ExpectedTotalPages int
+	}
+
+	for i, testCase := range []TestCase{
+		{
+			Name: "valid page and pageSize",
+			Page: 3, PageSize: 20, TotalItems: 100,
+			ExpectedOffset:     40,
+			ExpectedTotalPages: 5,
+		},
+		{
+			Name: "page exceeds total pages",
+			Page: 6, PageSize: 20, TotalItems: 100,
+			ExpectedOffset:     100,
+			ExpectedTotalPages: 5,
+		},
+		{
+			Name: "page equals total pages",
+			Page: 5, PageSize: 20, TotalItems: 100,
+			ExpectedOffset:     80,
+			ExpectedTotalPages: 5,
+		},
+		{
+			Name: "page and pageSize exceed total items",
+			Page: 3, PageSize: 50, TotalItems: 100,
+			ExpectedOffset:     100,
+			ExpectedTotalPages: 2,
+		},
+		{
+			Name: "page and pageSize equal total items",
+			Page: 2, PageSize: 50, TotalItems: 100,
+			ExpectedOffset:     50,
+			ExpectedTotalPages: 2,
+		},
+		{
+			Name: "page and pageSize less than total items",
+			Page: 1, PageSize: 50, TotalItems: 100,
+			ExpectedOffset:     0,
+			ExpectedTotalPages: 2,
+		},
+		{
+			Name: "page and pageSize less than total items",
+			Page: 1, PageSize: 50, TotalItems: 100,
+			ExpectedOffset:     0,
+			ExpectedTotalPages: 2,
+		},
+		{
+			Name: "total items exceeds page and pageSize by 1",
+			Page: 1, PageSize: 10, TotalItems: 11,
+			ExpectedOffset:     0,
+			ExpectedTotalPages: 2,
+		},
+		{
+			Name: "second page when total items exceeds page and pageSize by 1",
+			Page: 2, PageSize: 10, TotalItems: 11,
+			ExpectedOffset:     10,
+			ExpectedTotalPages: 2,
+		},
+	} {
+		if testResult := t.Run(testCase.Name, func(t *testing.T) {
+			offset, totalPages := processing.ParseOffsetAndTotalPages(
+				testCase.Page,
+				testCase.PageSize,
+				testCase.TotalItems,
+			)
+			require.Equalf(t,
+				testCase.ExpectedOffset, offset,
+				"Expected offset %d, got %d",
+				testCase.ExpectedOffset, offset,
+			)
+			require.Equalf(t,
+				testCase.ExpectedTotalPages, totalPages,
+				"Expected total pages %d, got %d",
+				testCase.ExpectedTotalPages, totalPages,
+			)
+		}); !testResult {
+			t.Errorf("Test case %d failed", i)
 		}
 	}
 }
